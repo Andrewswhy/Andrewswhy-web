7a4388ede44cb4957e7283ef2f391c9aec6c55d4
Andrew Yan <35612958+Andrewswhy@users.noreply.github.com>
2024-01-29 22:36:05 -0800

    Add files via upload

--- test-sketch.js
+++ test-sketch.js
@@ -1,47 +1,120 @@
-var canvas_x = 16*50;
-var canvas_y = 9*50;
-var count_x = 0;
-var count_y = 150;
-var direction_x = 1; // Direction variable to control movement direction
-var direction_y = 1;
+// Drawn from https://p5js.org/examples/simulate-penrose-tiles.html
+// This is a port by David Blitz of the "Penrose Tile" example from processing.org/examples
+
+var ds;
 
 function setup() {
-  createCanvas(canvas_x, canvas_y);
+  createCanvas(710, 400);
+  ds = new PenroseLSystem();
+  //please, play around with the following line
+  ds.simulate(5);
 }
 
 function draw() {
   background(0);
-  
-  var x = 0 + count_x;
-  var y = 0 + count_y;
-  
-  // Move the rectangle to the right if x + count is less than canvas_x
-  // Move the rectangle to the left if x + count is greater than canvas_x
-  count_x += 1.5 * direction_x;
-  count_y += 1.5 * direction_y;
-
-
-  if (count_x >= canvas_x - 200) {
-    // If the rectangle exceeds the right edge of the canvas
-    direction_x = -1;// Change direction to move left
-    fill(color(143,61,144))
-  } else if (count_x <= 0) {
-    // If the rectangle exceeds the left edge of the canvas
-    direction_x = 1; // Change direction to move right
-    fill(color(52,165,105))
-  }
-  
-  if (count_y >= canvas_y - 75) {
-    // If the rectangle exceeds the right edge of the canvas
-    direction_y = -1; // Change direction to move left
-    fill(color(94,17,17))
-  } else if (count_y <= 0) {
-    // If the rectangle exceeds the left edge of the canvas
-    direction_y = 1; // Change direction to move right
-    fill(color(225,165,165))
-  }
-
-  //rect(x, y, 200, 75);
-  textSize(100)
-  text("DVD", x-5, y+75)
+  ds.render();
 }
+
+function PenroseLSystem() {
+    this.steps = 0;
+
+   //these are axiom and rules for the penrose rhombus l-system
+   //a reference would be cool, but I couldn't find a good one
+    this.axiom = "[X]++[X]++[X]++[X]++[X]";
+    this.ruleW = "YF++ZF----XF[-YF----WF]++";
+    this.ruleX = "+YF--ZF[---WF--XF]+";
+    this.ruleY = "-WF++XF[+++YF++ZF]-";
+    this.ruleZ = "--YF++++WF[+ZF++++XF]--XF";
+
+    //please play around with the following two lines
+    this.startLength = 460.0;
+    this.theta = TWO_PI / 10.0; //36 degrees, try TWO_PI / 6.0, ...
+    this.reset();
+}
+
+PenroseLSystem.prototype.simulate = function (gen) {
+  while (this.getAge() < gen) {
+    this.iterate(this.production);
+  }
+}
+
+PenroseLSystem.prototype.reset = function () {
+    this.production = this.axiom;
+    this.drawLength = this.startLength;
+    this.generations = 0;
+  }
+
+PenroseLSystem.prototype.getAge = function () {
+    return this.generations;
+  }
+
+//apply substitution rules to create new iteration of production string
+PenroseLSystem.prototype.iterate = function() {
+    var newProduction = "";
+
+    for(var i=0; i<this.production.length; ++i) {
+      var step = this.production.charAt(i);
+      //if current character is 'W', replace current character
+      //by corresponding rule
+      if (step == 'W') {
+        newProduction = newProduction + this.ruleW;
+      }
+      else if (step == 'X') {
+        newProduction = newProduction + this.ruleX;
+      }
+      else if (step == 'Y') {
+        newProduction = newProduction + this.ruleY;
+      }
+      else if (step == 'Z') {
+        newProduction = newProduction + this.ruleZ;
+      }
+      else {
+        //drop all 'F' characters, don't touch other
+        //characters (i.e. '+', '-', '[', ']'
+        if (step != 'F') {
+          newProduction = newProduction + step;
+        }
+      }
+    }
+
+    this.drawLength = this.drawLength * 0.5;
+    this.generations++;
+    this.production = newProduction;
+}
+
+//convert production string to a turtle graphic
+PenroseLSystem.prototype.render = function () {
+    translate(width/2, height/2);
+
+    this.steps += 20;
+    if(this.steps > this.production.length) {
+      this.steps = this.production.length;
+    }
+
+    for(var i=0; i<this.steps; ++i) {
+      var step = this.production.charAt(i);
+
+      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
+      if( step == 'F') {
+        stroke(255, 60);
+        for(var j=0; j < this.repeats; j++) {
+          line(0, 0, 0, -this.drawLength);
+          noFill();
+          translate(0, -this.drawLength);
+        }
+        this.repeats = 1;
+      }
+      else if (step == '+') {
+        rotate(this.theta);
+      }
+      else if (step == '-') {
+        rotate(-this.theta);
+      }
+      else if (step == '[') {
+        push();
+      }
+      else if (step == ']') {
+        pop();
+      }
+    }
+  }
